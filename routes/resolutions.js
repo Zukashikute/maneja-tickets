adb6eb604e84a4a202c3458c6bd57c535afac77b
Nefi Perez <nefipr@gmail.com>
2023-11-24 00:44:12 +0100

    Routes created for the resolution

--- routes/resolutions.js
+++ routes/resolutions.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
-const ticketsController = require('../controllers/resolutions');
+const resolutionsController = require('../controllers/resolutions');
 
+// Create Resolution
+router.post('/resolutions', resolutionsController.createNewResolution);
 
+// Update Resolution using existing ID
+router.put('/resolutions/:id', resolutionsController.updateResolution);
+
+// Delete a Resolution by ID
+router.delete('/delete-resolution/:id', resolutionsController.deleteResolution);
 
 module.exports = router;
