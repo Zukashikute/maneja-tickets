2f81632ce22934ef0f07494f820a51d3cadc2f0a
Caleb Willden <carcabob@gmail.com>
2023-12-01 10:03:36 -0700

    Revised resolution and ticket models
    Tickets now have the priorityLevel, status, and assignedEmployee. Resolutions now have a dateCreated and resolvedByEmployee field, and a manually set ticketId

--- models/tickets.js
+++ models/tickets.js
@@ -1,48 +1,43 @@
 module.exports = (mongoose) => {
-const { ObjectId } = mongoose.Schema.Types;
-const Tickets = mongoose.model(
-'tickets',
-mongoose.Schema({
-    _id: {type: ObjectId, auto: true},
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true
-    },
-    jobPosition: {
-       type: String,
-       required: true  
-    },
-    ticketTitle: {
-        type: String,
-        required: true
-    },
-    ticketDescription: {
-        type: String,
-        required: true
-    },
-    // ticketId: {
-    //     type: Number
-    // },
-    dateCreated: {
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    priorityLevel: {
-        type: String,
-        enum: ['Low', 'Medium', 'High'],
-        default: 'Low',
-        required: true
-    },
-})    
-);
-return Tickets
-}
+    const { ObjectId } = mongoose.Schema.Types;
+    const Tickets = mongoose.model(
+        'tickets',
+        mongoose.Schema({
+            _id: { type: ObjectId, auto: true },
+            userId: {
+                type: ObjectId,
+                required: true,
+            },
+            title: {
+                type: String,
+                required: true,
+            },
+            description: {
+                type: String,
+                required: true,
+            },
+            priorityLevel: {
+                type: String,
+                enum: ['Low', 'Medium', 'High'],
+                default: 'Low',
+                required: true,
+            },
+            status: {
+                type: String,
+                enum: ['New', 'In Progress', 'Pending', 'Resolved', 'Closed'],
+                default: 'New',
+                required: true,
+            },
+            assignedEmployee: {
+                type: ObjectId,
+                required: true,
+            },
+            dateCreated: {
+                type: Date,
+                default: Date.now,
+                required: true,
+            },
+        })
+    );
+    return Tickets;
+};
